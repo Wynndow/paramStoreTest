74dc924b90819f7ecc8433ef4cd6b962d09b009c
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2022-03-07 11:47:46 +0000

    add pagination for basic client

--- src/main/java/Testy.java
+++ src/main/java/Testy.java
@@ -2,11 +2,14 @@ import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.GetParametersByPathRequest;
 import software.amazon.awssdk.services.ssm.model.GetParametersByPathResponse;
+import software.amazon.awssdk.services.ssm.model.Parameter;
 import software.amazon.lambda.powertools.parameters.ParamManager;
 import software.amazon.lambda.powertools.parameters.SSMProvider;
 
 import java.time.Instant;
 import java.time.temporal.ChronoField;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static software.amazon.awssdk.regions.Region.EU_WEST_2;
@@ -18,12 +21,17 @@ public class Testy {
 
         Instant beforeBasic = Instant.now();
 
-        GetParametersByPathRequest request = GetParametersByPathRequest.builder().path("/dev/core/credentialIssuers").recursive(true).build();
-        GetParametersByPathResponse response = ssmClient.getParametersByPath(request);
+        GetParametersByPathResponse response = ssmClient.getParametersByPath(buildRequestWithNextToken(null));
+        List<Parameter> fetchedParameters = new ArrayList<>(response.parameters());
+
+        while (response.nextToken() != null){
+            response = ssmClient.getParametersByPath(buildRequestWithNextToken(response.nextToken()));
+            fetchedParameters.addAll(response.parameters());
+        }
 
         Instant afterBasic = Instant.now();
 
-        System.out.printf("Parameters retrieved: %d%n", response.parameters().size());
+        System.out.printf("Parameters retrieved: %d%n", fetchedParameters.size());
 
 
         long beforeLong = (beforeBasic.getEpochSecond() * 1000) + beforeBasic.getLong(ChronoField.MILLI_OF_SECOND);
@@ -49,9 +57,17 @@ public class Testy {
         Instant afterPower = Instant.now();
 
         System.out.printf("Parameters retrieved: %d%n", powerParams.size());
-        
+
         long beforePowerLong = (beforePower.getEpochSecond() * 1000) + beforePower.getLong(ChronoField.MILLI_OF_SECOND);
         long afterPowerLong = (afterPower.getEpochSecond() * 1000) + afterPower.getLong(ChronoField.MILLI_OF_SECOND);
         System.out.printf("Duration of call to param store (ms): %d%n", afterPowerLong - beforePowerLong );
+    }
+
+    private static GetParametersByPathRequest buildRequestWithNextToken(String nextToken) {
+        return GetParametersByPathRequest.builder()
+                .path("/dev/core/credentialIssuers")
+                .recursive(true)
+                .nextToken(nextToken)
+                .build();
     }
 }
