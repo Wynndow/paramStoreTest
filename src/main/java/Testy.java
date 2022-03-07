b0ba146d6c4aa1e2d7c4b9972f9cdda529efb78f
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2022-03-07 10:48:56 +0000

    Add test for powertools

--- src/main/java/Testy.java
+++ src/main/java/Testy.java
@@ -1,28 +1,57 @@
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.GetParametersByPathRequest;
 import software.amazon.awssdk.services.ssm.model.GetParametersByPathResponse;
+import software.amazon.lambda.powertools.parameters.ParamManager;
+import software.amazon.lambda.powertools.parameters.SSMProvider;
 
 import java.time.Instant;
 import java.time.temporal.ChronoField;
+import java.util.Map;
 
 import static software.amazon.awssdk.regions.Region.EU_WEST_2;
 
 public class Testy {
     public static void main(String[] args) {
-        SsmClient ssmClient = SsmClient.builder().region(EU_WEST_2).build();
+        System.out.println("Testing basic client:");
+        SsmClient ssmClient = SsmClient.builder().region(EU_WEST_2).httpClient(UrlConnectionHttpClient.create()).build();
 
-        Instant before = Instant.now();
+        Instant beforeBasic = Instant.now();
 
         GetParametersByPathRequest request = GetParametersByPathRequest.builder().path("/dev/core/credentialIssuers").recursive(true).build();
         GetParametersByPathResponse response = ssmClient.getParametersByPath(request);
 
-        Instant after = Instant.now();
+        Instant afterBasic = Instant.now();
 
         System.out.printf("Parameters retrieved: %d%n", response.parameters().size());
 
 
-        long beforeLong = (before.getEpochSecond() * 1000) + before.getLong(ChronoField.MILLI_OF_SECOND);
-        long afterLong = (after.getEpochSecond() * 1000) + after.getLong(ChronoField.MILLI_OF_SECOND);
-        System.out.printf("Duration of call to param store (ms): %d%n", afterLong - beforeLong );
+        long beforeLong = (beforeBasic.getEpochSecond() * 1000) + beforeBasic.getLong(ChronoField.MILLI_OF_SECOND);
+        long afterLong = (afterBasic.getEpochSecond() * 1000) + afterBasic.getLong(ChronoField.MILLI_OF_SECOND);
+        System.out.printf("Duration of call to param store (ms): %d%n%n%n%n", afterLong - beforeLong );
+
+        System.out.println("Testing powertools client:");
+
+        SSMProvider ssmProvider = ParamManager.getSsmProvider(
+                SsmClient.builder()
+                        .httpClient(UrlConnectionHttpClient.create())
+                        .region(EU_WEST_2)
+                        .build());
+
+        Instant beforePower = Instant.now();
+
+        Map<String, String> powerParams =
+                ssmProvider
+                        .recursive()
+                        .getMultiple("/dev/core/credentialIssuers");
+
+
+        Instant afterPower = Instant.now();
+
+        System.out.printf("Parameters retrieved: %d%n", powerParams.size());
+        
+        long beforePowerLong = (beforePower.getEpochSecond() * 1000) + beforePower.getLong(ChronoField.MILLI_OF_SECOND);
+        long afterPowerLong = (afterPower.getEpochSecond() * 1000) + afterPower.getLong(ChronoField.MILLI_OF_SECOND);
+        System.out.printf("Duration of call to param store (ms): %d%n", afterPowerLong - beforePowerLong );
     }
 }
